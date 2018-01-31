3da02afe39e3364758e31ea9106a80a730c87df5
nsinghal7 <nmsinghal7@gmail.com>
2018-01-31 00:48:00 -0500

    Basic outline of organism

--- organism.h
+++ organism.h
@@ -1,3 +1,31 @@
+#ifndef ORGANISM_H
+#define ORGANISM_H
+
+#include "nutrient.h"
+#include "map.h"
+
+class OrganismType {
+    std::string name;
+    Nutrient primaryReqNutrient;
+    Nutrient primaryProdNutrient;
+    int createAmount;
+    int drainAmount;
+    int consumeAmount;
+    int primaryProdAmount;
+    int otherProdAmount;
+public:
+    OrganismType(){};
+    double scoreCreationOnSquare(MapSquare &square);
+    friend std::istream &operator>>(std::istream &is, OrganismType &ot);
+    friend std::ostream &operator<<(std::ostream &os, const OrganismType &ot);
+    friend Organism;
+};
+
 class Organism {
-    
-}
+    std::shared_ptr<OrganismType> ot;
+    Nutrient secondaryReqNutrient;
+public:
+    Organism(std::shared_ptr<OrganismType> ot, Nutrient &secondaryReqNutrient): ot(ot), secondaryReqNutrient(secondaryReqNutrient){};
+};
+
+#endif
