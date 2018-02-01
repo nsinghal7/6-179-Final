643624bd1f8af128127eb1f4bd94baa85e2d424c
nsinghal7 <nmsinghal7@gmail.com>
2018-01-31 23:21:27 -0500

    Re-add cout instructions

--- organism.cpp
+++ organism.cpp
@@ -16,13 +16,21 @@ double OrganismType::scoreCreationOnSquare(std::vector<NutrientAndAmount> &nutri
 }
 
 std::istream &operator>>(std::istream &is, OrganismType &ot) {
+    std::cout << "Name of organism (1 char): ";
     is >> ot.name;
+    std::cout << "Primary consumption nutrient name: ";
     is >> ot.primaryReqNutrient;
+    std::cout << "Primary production nutrient name: ";
     is >> ot.primaryProdNutrient;
+    std::cout << "Primary nutrient cost for creation: ";
     is >> ot.createAmount;
+    std::cout << "Primary nutrient drain amount per square per turn: ";
     is >> ot.drainAmount;
+    std::cout << "Primary/secondary nutrient consumption amount per turn: ";
     is >> ot.consumeAmount;
+    std::cout << "Primary nutrient production amount per turn: ";
     is >> ot.primaryProdAmount;
+    std::cout << "Other nutrient production amount per nutrient per turn: ";
     is >> ot.otherProdAmount;
     return is;
 }
